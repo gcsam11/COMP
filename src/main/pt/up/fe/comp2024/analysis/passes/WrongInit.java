891af6e3f694a07773671ffef26b4e89a4e21c11
Gonçalo Martins <goncalo.csam@gmail.com>
2024-04-18 20:39:15 +0100

    more tests

--- src/main/pt/up/fe/comp2024/analysis/passes/WrongInit.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/WrongInit.java
@@ -207,7 +207,8 @@ public class WrongInit extends AnalysisVisitor {
     private Void visitAssignStmt(JmmNode assignStmt, SymbolTable table) {
         SpecsCheck.checkNotNull(currentMethod, () -> "Expected current method to be set");
 
-        if(!assignStmt.getChild(0).getKind().equals(Kind.IDENTIFIER.getNodeName())){
+        if(!assignStmt.getChild(0).getKind().equals(Kind.IDENTIFIER.getNodeName())
+                && !assignStmt.getChild(0).getKind().equals(Kind.ARRAY_ACCESS_OP.getNodeName())){
             var message = "Left side of assignment statement should be an identifier";
             addReport(Report.newError(
                     Stage.SEMANTIC,
