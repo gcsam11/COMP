2a4b62ccd9473766ad04ef5ddaacba8f5ae1c31e
Gonçalo Martins <goncalo.csam@gmail.com>
2024-04-19 22:23:57 +0100

    more tests

--- src/main/pt/up/fe/comp2024/optimization_jasmin/JasminExprGeneratorVisitor.java
+++ src/main/pt/up/fe/comp2024/optimization_jasmin/JasminExprGeneratorVisitor.java
@@ -114,6 +114,11 @@ public class JasminExprGeneratorVisitor extends PostorderJmmVisitor<StringBuilde
             default -> throw new NotImplementedException(binaryExpr.get("op"));
         };
 
+        if(binaryExpr.getChildren(Kind.BINARY_EXPR).size() > 0){
+            code.append(op).append(NL);
+            return null;
+        }
+
         //Added this because binary expression does not store the variable in the stack before running the code
         if (!binaryExpr.getParent().getKind().equals("ParenOp")  && !binaryExpr.getParent().getKind().equals("BinaryExpr")) {
             code.append("ldc 0").append(NL);
